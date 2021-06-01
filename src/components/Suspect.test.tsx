49cb63c0bea69d3675f8804171168b45c7430ae6
Brett Kinnamon <me@brettk.dev>
2021-06-01 15:59:33 -0400

    change the method for getting the suspect component in the tests

--- src/components/Suspect.test.tsx
+++ src/components/Suspect.test.tsx
@@ -1,48 +1,48 @@
-import {render, screen} from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
-import {Suspect} from './Suspect'
+import { Suspect } from './Suspect'
 
 describe('Suspect', () => {
-  it('renders learn react link', () => {
-    render(<Suspect name="SuspectName" />)
-    const liElement = screen.getByText('SuspectName')
+    it('renders learn react link', () => {
+        render(<Suspect name="SuspectName" />)
+        const liElement = screen.getByText('SuspectName')
 
-    expect(liElement).toBeInTheDocument()
-    expect(liElement.tagName).toBe('LI')
-  })
+        expect(liElement).toBeInTheDocument()
+        expect(liElement.tagName).toBe('LI')
+    })
 
-  it('emits an is cleared change request when starting with false and is clicked', () => {
-    const setIsCleared = jest.fn()
-    render(<Suspect name="SuspectName" isCleared={false} setIsCleared={setIsCleared} />)
-    const liElement = screen.getByText('SuspectName')
-    liElement.click()
+    it('emits an is cleared change request when starting with false and is clicked', () => {
+        const setIsCleared = jest.fn()
+        render(<Suspect name="SuspectName" isCleared={false} setIsCleared={setIsCleared} />)
+        const liElement = screen.getByTestId('suspect')
+        liElement.click()
 
-    expect(setIsCleared).toHaveBeenCalledWith(true)
-  })
+        expect(setIsCleared).toHaveBeenCalledWith(true)
+    })
 
-  it('emits a not cleared change request when starting with true and clicked', () => {
-    const setIsCleared = jest.fn()
-    render(<Suspect name="SuspectName" isCleared={true} setIsCleared={setIsCleared} />)
-    const liElement = screen.getByText('SuspectName')
-    liElement.click()
+    it('emits a not cleared change request when starting with true and clicked', () => {
+        const setIsCleared = jest.fn()
+        render(<Suspect name="SuspectName" isCleared={true} setIsCleared={setIsCleared} />)
+        const liElement = screen.getByTestId('suspect')
+        liElement.click()
 
-    expect(setIsCleared).toHaveBeenCalledWith(false)
-  })
+        expect(setIsCleared).toHaveBeenCalledWith(false)
+    })
 
-  it('has the base class only when rendered with as cleared=false', () => {
-    render(<Suspect name="SuspectName" isCleared={false} />)
-    const liElement = screen.getByText('SuspectName')
+    it('has the base class only when rendered with as cleared=false', () => {
+        render(<Suspect name="SuspectName" isCleared={false} />)
+        const liElement = screen.getByTestId('suspect')
 
-    expect(liElement.classList).toHaveLength(1)
-    expect(liElement.classList.contains('suspect-list__suspect')).toBe(true)
-  })
+        expect(liElement.classList).toHaveLength(1)
+        expect(liElement.classList.contains('suspect-list__suspect')).toBe(true)
+    })
 
-  it('has the base and cleared classes when rendered with as cleared=true', () => {
-    render(<Suspect name="SuspectName" isCleared={true} />)
-    const liElement = screen.getByText('SuspectName')
+    it('has the base and cleared classes when rendered with as cleared=true', () => {
+        render(<Suspect name="SuspectName" isCleared={true} />)
+        const liElement = screen.getByTestId('suspect')
 
-    expect(liElement.classList).toHaveLength(2)
-    expect(liElement.classList.contains('suspect-list__suspect')).toBe(true)
-    expect(liElement.classList.contains('suspect-list__suspect--cleared')).toBe(true)
-  })
+        expect(liElement.classList).toHaveLength(2)
+        expect(liElement.classList.contains('suspect-list__suspect')).toBe(true)
+        expect(liElement.classList.contains('suspect-list__suspect--cleared')).toBe(true)
+    })
 })
