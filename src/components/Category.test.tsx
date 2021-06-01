cdf7f7144c8b1ebae985f1d615763dfe2d21b6fb
Brett Kinnamon <me@brettk.dev>
2021-06-01 16:46:30 -0400

    refactor the category header

--- src/components/Category.test.tsx
+++ src/components/Category.test.tsx
@@ -11,12 +11,13 @@ describe('Category', () => {
     expect(categoryElement.tagName).toBe('UL')
   })
 
-  it('renders a list item with the name in it', () => {
+  it('has a header with an indicator and a title', () => {
     render(<Category name="People" />)
-    const headerElement = screen.getByText('People')
+    const indicatorElement = screen.queryByText('-')
+    const titleElement = screen.queryByText('People')
 
-    expect(headerElement).toBeInTheDocument()
-    expect(headerElement.tagName).toBe('LI')
+    expect(indicatorElement).toBeInTheDocument()
+    expect(titleElement).toBeInTheDocument()
   })
 
   it('renders a given list of suspects', () => {
@@ -30,21 +31,43 @@ describe('Category', () => {
   it('hides the suspects when the header is clicked', () => {
     const suspects = ['Green', 'Mustard', 'Peacock']
     render(<Category name="People" suspects={suspects} />)
-    const headerElement = screen.getByText('People')
+    const headerElement = screen.getByTestId('category-header')
     headerElement.click()
+    const indicatorElement = screen.queryByText('+')
     const suspectElements = screen.queryAllByTestId('suspect')
 
+    expect(indicatorElement).toBeInTheDocument()
     expect(suspectElements).toHaveLength(0)
   })
 
   it('shows the suspects when the header is clicked twice', () => {
     const suspects = ['Green', 'Mustard', 'Peacock']
     render(<Category name="People" suspects={suspects} />)
-    const headerElement = screen.getByText('People')
+    const headerElement = screen.getByTestId('category-header')
     headerElement.click()
     headerElement.click()
+    const indicatorElement = screen.queryByText('-')
     const suspectElements = screen.queryAllByTestId('suspect')
 
+    expect(indicatorElement).toBeInTheDocument()
     expect(suspectElements).toHaveLength(3)
+  })
+
+  it('has the suspect-list class', () => {
+    render(<Category />)
+    const categoryElement = screen.getByTestId('category')
+
+    expect(categoryElement.classList).toHaveLength(1)
+    expect(categoryElement.classList.contains('suspect-list')).toBe(true)
+  })
+
+  it('renders a list item with the name in it', () => {
+    render(<Category name="People" />)
+    const headerElement = screen.getByTestId('category-header')
+
+    expect(headerElement).toBeInTheDocument()
+    expect(headerElement.tagName).toBe('LI')
+    expect(headerElement.classList).toHaveLength(1)
+    expect(headerElement.classList.contains('suspect-list__header'))
   })
 })
--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -12,9 +12,25 @@ export const Category = (props: Props) => {
 
   const [open, setOpen] = useState(true)
 
+  const listClass = 'suspect-list'
+  const headerClass = `${listClass}__header`
+  const indicatorClass = `${listClass}__indicator`
+  const titleClass = `${listClass}__title`
+
   return (
-    <ul data-testid="category">
-      <li onClick={() => setOpen(!open)}>{name}</li>
+    <ul className={listClass} data-testid="category">
+      <li
+        className={headerClass}
+        onClick={() => setOpen(!open)}
+        data-testid="category-header"
+      >
+        <div className={indicatorClass}>
+          {open ? '-' : '+'}
+        </div>
+        <div className={titleClass}>
+          {name}
+        </div>
+      </li>
       {open && suspects.map(suspect => (
         <Suspect key={suspect} name={suspect} />
       ))}
